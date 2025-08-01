72f872d415527737261e0b5200bda3948ce2d58e
Ruben Arts <ruben.arts@hotmail.com>
2025-08-01 13:31:46 +0200

    fix: reformat and fix ROS1 package issues

--- backends/pixi-build-ros/src/pixi_build_ros/ros_generator.py
+++ backends/pixi-build-ros/src/pixi_build_ros/ros_generator.py
@@ -9,7 +9,7 @@ from pixi_build_backend.types.generated_recipe import (
     GenerateRecipeProtocol,
     GeneratedRecipe,
 )
-from pixi_build_backend.types.intermediate_recipe import NoArchKind, Python, Script, ConditionalRequirements, \
+from pixi_build_backend.types.intermediate_recipe import Script, ConditionalRequirements, \
     ItemPackageDependency, Package
 from pixi_build_backend.types.platform import Platform
 from pixi_build_backend.types.project_model import ProjectModelV1
@@ -17,13 +17,9 @@ from pixi_build_backend.types.python_params import PythonParams
 
 from .build_script import BuildScriptContext, BuildPlatform
 from .distro import Distro
-from .utils import get_build_input_globs, get_editable_setting
+from .utils import get_build_input_globs, package_xml_to_conda_requirements, convert_package_xml_to_catkin_package, \
+    get_package_xml_content
 
-from catkin_pkg.package import Package as CatkinPackage, parse_package_string
-import os
-import yaml
-from itertools import chain
-from pprint import pprint
 
 @dataclass
 class ROSBackendConfig:
@@ -171,87 +167,5 @@ class ROSGenerator(GenerateRecipeProtocol):
         """Extract input globs for the build."""
         return get_build_input_globs(config, editable)
 
-
-def get_package_xml_content(manifest_root: Path) -> str:
-    """Read package.xml file from the manifest root."""
-    package_xml_path = manifest_root / "package.xml"
-    if not package_xml_path.exists():
-        raise FileNotFoundError(f"package.xml not found at {package_xml_path}")
-
-    with open(package_xml_path, 'r') as f:
-        return f.read()
-
-def convert_package_xml_to_catkin_package(package_xml_content: str) -> CatkinPackage:
-    """Convert package.xml content to a CatkinPackage object."""
-    package_reading_warnings = None
-    package_xml = parse_package_string(package_xml_content, package_reading_warnings)
-
-    # Evaluate conditions in the package.xml
-    # TODO: validate the need for dealing with configuration conditions
-    package_xml.evaluate_conditions(os.environ)
-
-    return package_xml
-
-def rosdep_to_conda_package_name(dep_name: str, distro: Distro) -> List[str]:
-    """Convert a ROS dependency name to a conda package name."""
-    # If dependency any of the following return custom name:
-    if dep_name in ["ament_cmake", "ament_python", "rosidl_default_generators"]:
-        return [f"ros-{distro.name}-{dep_name.replace('_', '-')}"]
-
-    # TODO: Currently hardcoded and not able to override, this should be configurable
-    with open(Path(__file__).parent.parent.parent / "robostack.yaml") as f:
-        # Parse yaml file into dict
-        robostack_data = yaml.safe_load(f)
-
-    if dep_name not in robostack_data:
-        # If the dependency is not found in robostack.yaml, check the actual distro whether it exists
-        if distro.has_package(dep_name):
-            # This means that it is a ROS package, so we are going to assume has the `ros-<distro>-<dep_name>` format.
-            return [f"ros-{distro.name}-{dep_name.replace('_', '-')}"]
-        else:
-            # If the dependency is not found in robostack.yaml and not in the distro, return the dependency name as is.
-            return [dep_name]
-
-    # Dependency found in robostack.yaml
-    # Get the conda packages for the dependency
-    conda_packages = robostack_data[dep_name].get("robostack", [])
-
-    if isinstance(conda_packages, dict):
-        # TODO: Handle different platforms
-        conda_packages = conda_packages.get("linux", [])
-
-    return conda_packages
-
-def package_xml_to_conda_requirements(
-    pkg: CatkinPackage,
-    distro: Distro,
-) -> ConditionalRequirements:
-
-    build_deps = pkg.buildtool_depends
-    build_deps += pkg.buildtool_export_depends
-    build_deps += pkg.build_depends
-    build_deps += pkg.build_export_depends
-    build_deps = [d.name for d in build_deps if d.evaluated_condition]
-    build_deps += ["ros_workspace"]
-    conda_build_deps = [rosdep_to_conda_package_name(dep, distro) for dep in build_deps]
-    conda_build_deps = list(dict.fromkeys(chain.from_iterable(conda_build_deps)))
-
-    run_deps = pkg.run_depends
-    run_deps += pkg.exec_depends
-    run_deps += pkg.build_export_depends
-    run_deps += pkg.buildtool_export_depends
-    run_deps = [d.name for d in run_deps if d.evaluated_condition]
-    conda_run_deps = [rosdep_to_conda_package_name(dep, distro) for dep in run_deps]
-    conda_run_deps = list(dict.fromkeys(chain.from_iterable(conda_run_deps)))
-
-    build_requirements = [ItemPackageDependency(name) for name in conda_build_deps]
-    run_requirements = [ItemPackageDependency(name) for name in conda_run_deps]
-
-    cond = ConditionalRequirements()
-    cond.host = build_requirements
-    cond.build = build_requirements
-    cond.run = run_requirements
-
-    return cond
 
 
